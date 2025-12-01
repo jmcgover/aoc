a2844486243c6c56ccaab517d2ef1b32b7b2d246
Jeffrey McGovern <jeff@jdmcg.org>
2025-12-01 04:06:03 -0800

    split apart part 1 and 2

--- 2025/1/part2.py
+++ 2025/1/part2.py
@@ -44,32 +44,6 @@ def method_0x434C49434B(
     return pointings
 
 
-def count_landings(
-    input: TextIO,
-    val: int,
-    start_val: int,
-    num_vals: int,
-) -> int:
-    """Count the number of times the dial lands on a value after any rotation."""
-    cur_val: int = start_val
-    landings: int = 0 if cur_val != val else 1
-    for line in input:
-        distance: int = int(line[1:])
-        direction = Direction(line[0])
-        match direction:
-            case Direction.left:
-                cur_val = (cur_val - distance) % num_vals
-            case Direction.right:
-                cur_val = (cur_val + distance) % num_vals
-            case _:
-                assert_never(direction)
-        assert cur_val >= 0
-        assert cur_val < num_vals
-        if cur_val == val:
-            landings += 1
-    return landings
-
-
 def main() -> int:
     with sys.stdin as input:
         password: int = method_0x434C49434B(
