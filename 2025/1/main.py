f276ee6cf0bca57d6add097881e8b2addcebc167
Jeffrey McGovern <jeff@jdmcg.org>
2025-12-01 03:44:34 -0800

    it works -- don't be clever, I guess

--- 2025/1/main.py
+++ 2025/1/main.py
@@ -11,7 +11,8 @@ class Direction(StrEnum):
     left = "L"
     right = "R"
 
-def method_0x434C49434B(
+
+def method_0x434C49434B_wrong(
     input: TextIO,
     start_val: int,
     num_vals: int,
@@ -65,6 +66,38 @@ def method_0x434C49434B(
     return crossings + landings
 
 
+def method_0x434C49434B(
+    input: TextIO,
+    val: int,
+    start_val: int,
+    num_vals: int,
+) -> int:
+    """Count the number of times the dial lands on a value after any rotation."""
+    cur_val: int = start_val
+    pointings: int = 0 if cur_val != val else 1
+    for line in input:
+        distance: int = int(line[1:])
+        direction = Direction(line[0])
+        match direction:
+            case Direction.left:
+                while distance > 0:
+                    cur_val = (cur_val - 1) % num_vals
+                    if cur_val == 0:
+                        pointings += 1
+                    distance -= 1
+            case Direction.right:
+                while distance > 0:
+                    cur_val = (cur_val + 1) % num_vals
+                    if cur_val == 0:
+                        pointings += 1
+                    distance -= 1
+            case _:
+                assert_never(direction)
+        assert cur_val >= 0
+        assert cur_val < num_vals
+    return pointings
+
+
 def count_landings(
     input: TextIO,
     val: int,
@@ -95,6 +128,7 @@ def main() -> int:
     with sys.stdin as input:
         password: int = method_0x434C49434B(
             input=input,
+            val=0,
             start_val=50,
             num_vals=100,
         )
